46a3fa87919a853554f9925ab6fcc5cde2684d0b
skpixels <makkobir79@gmail.com>
2023-08-21 22:11:52 +0300

    hero section completed

--- components/hero.tsx
+++ components/hero.tsx
@@ -3,10 +3,20 @@ import hero from "../public/assets/hero-image.jpg";
 
 function HeroPage() {
   return (
-    <div
-      style={{ backgroundImage: `url(${hero})` }}
-      className="heroContainer"
-    ></div>
+    <div style={{ backgroundImage: `url(${hero})` }} className="heroContainer">
+      <div className="d-flex justify-content-center align-items-center flex-column">
+        <h1 className="heroContainerTitle">
+          I’m a <span style={{ color: "#FF4900" }}>Graphic Designer</span>
+        </h1>
+        <p className="heroContainerParagraph">
+          A wonderful serenity has taken possession of my entire soul,
+          <br /> like these sweet mornings of spring which I enjoy <br /> with
+          my whole heart.ree heart.
+        </p>
+
+        <button className="primaryBtn">Hire Me</button>
+      </div>
+    </div>
   );
 }
 
