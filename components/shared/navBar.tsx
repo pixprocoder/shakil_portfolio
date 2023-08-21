ae1492e900307d6859b647916bc37a8944fd5c85
skpixels <makkobir79@gmail.com>
2023-08-22 00:20:13 +0300

    fix header

--- components/shared/navBar.tsx
+++ components/shared/navBar.tsx
@@ -3,29 +3,27 @@ import Link from "next/link";
 
 export const NavBar = () => {
   return (
-    <header>
-      <div>
-        <nav className=" d-flex justify-content-between align-items-center">
-          <div className="logo">LOGO</div>
-          <div className=" ">
-            <Link className="navItem" href="/">
-              Home
-            </Link>
-            <Link className="navItem" href="/">
-              About
-            </Link>
-            <Link className="navItem" href="/">
-              Portfolio
-            </Link>
-            <Link className="navItem" href="/">
-              Contact
-            </Link>
-            <Link className="" href="/">
-              <button className="primaryBtn">Hire Me</button>
-            </Link>
-          </div>
-        </nav>
-      </div>
+    <header className="pt-3">
+      <nav className=" d-flex justify-content-between align-items-center">
+        <div className="logo">LOGO</div>
+        <div className=" ">
+          <Link className="navItem" href="/">
+            Home
+          </Link>
+          <Link className="navItem" href="/">
+            About
+          </Link>
+          <Link className="navItem" href="/">
+            Portfolio
+          </Link>
+          <Link className="navItem" href="/">
+            Contact
+          </Link>
+          <Link className="" href="/">
+            <button className="primaryBtn">Hire Me</button>
+          </Link>
+        </div>
+      </nav>
     </header>
   );
 };
