9f0e547e84fb63849e605c9c12b62b19c38cc410
skpixels <makkobir79@gmail.com>
2023-08-21 22:02:04 +0300

    overview card added

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import HeroPage from "@/components/hero";
+import OverviewCardPage from "@/components/overviewCard";
 import { NavBar } from "@/components/shared/navBar";
 
 export default function HomePage() {
@@ -10,6 +11,9 @@ export default function HomePage() {
       <div>
         <HeroPage />
       </div>
+      <section className="section">
+        <OverviewCardPage />
+      </section>
     </div>
   );
 }
---
+++ components/overviewCard.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+function OverviewCardPage() {
+  return (
+    <div className="overviewCardContainer">
+      <div className="d-flex justify-content-between align-items-center">
+        <div>
+          <h1 className="overviewCardTitle">50+</h1>
+          <p className="overviewCardParagraph">Clients</p>
+        </div>
+        <div>
+          <h1 className="overviewCardTitle">30</h1>
+          <p className="overviewCardParagraph">Projects</p>
+        </div>
+        <div>
+          <h1 className="overviewCardTitle">48</h1>
+          <p className="overviewCardParagraph">Awards</p>
+        </div>
+        <div>
+          <h1 className="overviewCardTitle">06</h1>
+          <p className="overviewCardParagraph">Experiences</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default OverviewCardPage;
