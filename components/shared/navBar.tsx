160412f99e7e490c4a295e859bdc2e466afd8316
samsul kobir <makkobir79@gmail.com>
2023-11-04 18:18:10 +0200

    menu bar responsive completed

--- components/shared/navBar.tsx
+++ components/shared/navBar.tsx
@@ -1,15 +1,28 @@
 "use client";
 import Link from "next/link";
 import { Button } from "../ui/button";
+import { useState } from "react";
 
 export const NavBar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  console.log(isOpen);
+
   return (
     <header className="pt-3">
-      <nav className=" flex justify-between items-center flex-col lg:flex-row">
-        <div className="logo hidden lg:block">
+      <nav className=" flex justify-between items-center relative lg:flex-row">
+        <div className="logo flex ">
           <Link href="/">Shakil</Link>
         </div>
-        <div className="flex gap-6 items-center">
+        <div className="block lg:hidden">
+          <Button onClick={() => setIsOpen(!isOpen)}>Menu</Button>
+        </div>
+        <div
+          className={`${
+            isOpen
+              ? "flex flex-col absolute top-10 w-full transition-all duration-300 ease-in-out z-10 bg-black"
+              : "hidden"
+          }  lg:flex gap-6 items-center`}
+        >
           <Link className="navItem" href="/">
             Home
           </Link>
